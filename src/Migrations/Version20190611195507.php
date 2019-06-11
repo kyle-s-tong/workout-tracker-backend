aebda7d535ec12d84afd10359a575a68214a557b
Kyle <kyle.simon.tong@gmail.com>
2019-06-12 07:55:45 +1200

    Add details and delete old migrations as I move to JSONAPI spec

--- src/Migrations/Version20190611195507.php
+++ src/Migrations/Version20190611195507.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -8,15 +10,21 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190425054115 extends AbstractMigration
+final class Version20190611195507 extends AbstractMigration
 {
+    public function getDescription() : string
+    {
+        return '';
+    }
+
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, first_name VARCHAR(255) DEFAULT NULL, last_name VARCHAR(255) DEFAULT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE token (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, value VARCHAR(191) NOT NULL, enabled TINYINT(1) NOT NULL, expiry_date DATETIME NOT NULL, created DATETIME NOT NULL, updated DATETIME DEFAULT NULL, INDEX IDX_5F37A13BA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE routine (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE exercise (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE exercise_workout (exercise_id INT NOT NULL, workout_id INT NOT NULL, INDEX IDX_BE8A0C37E934951A (exercise_id), INDEX IDX_BE8A0C37A6CCCFC9 (workout_id), PRIMARY KEY(exercise_id, workout_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE workout (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, title VARCHAR(255) NOT NULL, INDEX IDX_649FFB72A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
@@ -35,8 +43,9 @@ final class Version20190425054115 extends AbstractMigration
         $this->addSql('ALTER TABLE workout DROP FOREIGN KEY FK_649FFB72A76ED395');
         $this->addSql('ALTER TABLE exercise_workout DROP FOREIGN KEY FK_BE8A0C37E934951A');
         $this->addSql('ALTER TABLE exercise_workout DROP FOREIGN KEY FK_BE8A0C37A6CCCFC9');
-        $this->addSql('DROP TABLE token');
         $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE token');
+        $this->addSql('DROP TABLE routine');
         $this->addSql('DROP TABLE exercise');
         $this->addSql('DROP TABLE exercise_workout');
         $this->addSql('DROP TABLE workout');
