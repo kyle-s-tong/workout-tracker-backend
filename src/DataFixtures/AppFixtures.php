cf6af22dfadd3f195697846e85a4c5ff1c792aa2
Kyle <kyle.simon.tong@gmail.com>
2019-06-15 13:07:28 +1200

    Add config changes to make fixtures work and the password/token hasher

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+      $user = new User();
+
+      $user->setEmail('kyle@test.com');
+      $user->setPassword('123test123');
+
+      $manager->persist($user);
+
+      $manager->flush();
+    }
+}
